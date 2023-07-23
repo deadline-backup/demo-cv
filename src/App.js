2138d2217bcbc8371893e1a4af31958420c4df39
deadline-backup <timerush123@gmail.com>
2023-07-23 19:51:53 +0700

    modified:   src/App.js 	modified:   src/demo.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './demo.css';
 import DemoProfile from './DemoProfile';
 function App() {
   return (
-    <div className="App">
+    <div className="profile">
       {/* Use the DemoProfile component */}
       <DemoProfile />
     </div>
